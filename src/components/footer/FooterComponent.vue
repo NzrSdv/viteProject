d334a90e521ad51dd6abec2453ec4bbb9980d9d2
Nazar <nzrsdv17@gmail.com>
2025-03-23 16:58:39 +0500

    ended the footer and main page but flexible

--- src/components/footer/FooterComponent.vue
+++ src/components/footer/FooterComponent.vue
@@ -1,18 +1,90 @@
+<script setup>
+import FooterLinksComponent from "./FooterLinksComponent.vue";
+</script>
 <template>
-    <footer class="footer">
-<div class="container"></div>
-    </footer>
+  <footer class="footer">
+    <div class="container">
+      <div class="left">
+        <h2 class="title">Присоединяйтесь</h2>
+      </div>
+      <div class="right">
+        <FooterLinksComponent v-for="(item, index) in footerInfo" :key="index" :info="item"/>
+      </div>
+    </div>
+  </footer>
 </template>
 <script>
 export default {
-    
-}
+  name: "FooterComponent",
+  components: { FooterLinksComponent },
+  data() {
+    return {
+      footerInfo: [
+        {
+          title: "каталог",
+          links: [
+            { name: "Шкафы (МДФ)", link: "" },
+            { name: "Шкафы (распашные)", link: "" },
+            { name: "Шкафы (купе)", link: "" },
+            { name: "Спальные гарнитуры", link: "" },
+            { name: "Кровати", link: "" },
+            { name: "Диваны", link: "" },
+            { name: "Пуфики", link: "" },
+            { name: "Кухонные гарнитуры", link: "" },
+            { name: "Столы", link: "" },
+            { name: "Прихожие", link: "" },
+            { name: "Комоды", link: "" },
+            { name: "Стеллажи", link: "" },
+            { name: "Б/У", link: "" },
+          ],
+        },
+        {
+          title: "навигация",
+          links: [
+            { name: "О компании", link: "" },
+            { name: "Акции", link: "" },
+            { name: "Доставка и оплата", link: "" },
+          ],
+        },
+        {
+          title: "контакты",
+          links: [
+            { name: "Cвяжитесь с нами", link: "" },
+          ],
+        },
+      ],
+    };
+  },
+};
 </script>
-<style>
-    .footer{
-        margin-top: 100px;
-        min-height: 346px;
-        background-color: #141414;
-
-    }
+<style scoped>
+.footer {
+  margin-top: 100px;
+  min-height: 346px;
+  background-color: #141414;
+  padding: 34px 57px;
+}
+.footer .container{
+    display: flex;
+    justify-content: flex-start;
+    gap:125px;
+    flex-wrap: wrap;
+    padding-bottom: 38px;
+    border-bottom: 1px solid #FFFFFF1A;
+}
+.title {
+  color: white;
+  font-family: "Raleway";
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 30px;
+  letter-spacing: 0%;
+}
+.right{
+    display:flex;
+    gap:60px;
+    justify-content: center;
+    align-items: flex-start;
+    flex-wrap: wrap;
+}
 </style>
---
+++ src/components/footer/FooterLinksComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <h3 class="links-title">{{ info.title }}</h3>
+    <ul class="links">
+      <li v-for="(item, index) in info.links" :key="index">
+        <a :href="item.link">{{ item.name }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "FooterLinksComponent",
+  props: {
+    info: Object,
+  },
+};
+</script>
+<style scoped>
+.wrapper {
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap:18px;
+}
+.links-title {
+  font-family: "Raleway";
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 2%;
+  text-transform: uppercase;
+}
+ul.links {
+  max-height: 180px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  column-gap: 43px;
+  row-gap: 7px;
+  flex-wrap: wrap;
+}
+ul li a {
+  font-family: 'Raleway';
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 18px;
+  letter-spacing: 0%;
+}
+</style>
