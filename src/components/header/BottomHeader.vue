c2186fdcfce8878df418a189f7375df463c917e8
Nazar <nzrsdv17@gmail.com>
2025-04-09 20:04:59 +0500

    added main to the nav

--- src/components/header/BottomHeader.vue
+++ src/components/header/BottomHeader.vue
@@ -2,7 +2,8 @@
   <div
     class="pt-2 px-4 border-t border-solid border-light-445 flex items-center justify-start"
   >
-    <div v-if="status"
+    <div
+      v-if="status"
       class="fixed w-screen h-screen bg-black/20 top-0 left-0 md:hidden z-50"
     ></div>
     <nav class="flex md:flex-row flex-col md:gap-8">
@@ -16,25 +17,30 @@
       </div>
       <ul
         :class="[
-          'fixed top-0 left-0 px-3 h-screen md:h-min bg-white md:bg-transparent md:static flex flex-col md:flex-row z-60 p-2 rounded items-center pt-10 md:pt-0 gap-10 md:gap-8 justify-start md:justify-center text-dark-05 font-[Raleway] font-medium text-base/6',
+          'fixed top-0 left-0 px-3 h-screen md:h-min bg-white md:bg-transparent md:static flex flex-col md:flex-row z-60 p-2 rounded items-center pt-10 md:pt-0 gap-6 md:gap-8 justify-start md:justify-center text-dark-05 font-[Raleway] font-medium text-base/6',
           status ? 'md:flex' : 'md:flex hidden',
         ]"
       >
         <li
+          v-if="$route.path != '/'"
           @click="() => (status = !status)"
-          class="md:border-none border-b border-solid px-4 border-black/20 w-full text-center hover:bg-black/20 duration-300"
+          class="py-4 md:border-none border-b border-solid px-4 border-black/20 w-full text-center hover:bg-black/20 duration-300"
+        ><router-link to="/">Главная</router-link></li>
+        <li
+          @click="() => (status = !status)"
+          class="py-4 md:border-none border-b border-solid px-4 border-black/20 w-full text-center hover:bg-black/20 duration-300"
         >
           <router-link to="/catalog/Шкафы (МДФ)">Каталог товаров</router-link>
         </li>
         <li
           @click="() => (status = !status)"
-          class="md:border-none border-b border-solid px-4 border-black/20 w-full text-center hover:bg-black/20 duration-300"
+          class="py-4 md:border-none border-b border-solid px-4 border-black/20 w-full text-center hover:bg-black/20 duration-300"
         >
           <router-link to="/catalog/Шкафы (МДФ)">Акции</router-link>
         </li>
         <li
           @click="() => (status = !status)"
-          class="md:border-none border-b border-solid px-4 border-black/20 w-full text-center hover:bg-black/20 duration-300"
+          class="py-4 md:border-none border-b border-solid px-4 border-black/20 w-full text-center hover:bg-black/20 duration-300"
         >
           <router-link :to="{ path: $route.path, hash: '#footerAnchor' }"
             >О нас</router-link
@@ -42,14 +48,14 @@
         </li>
         <li
           @click="() => (status = !status)"
-          class="md:border-none border-b border-solid px-4 border-black/20 w-full text-center hover:bg-black/20 duration-300"
+          class="py-4 md:border-none border-b border-solid px-4 border-black/20 w-full text-center hover:bg-black/20 duration-300"
         >
           <router-link :to="{ path: $route.path, hash: '#footerAnchor' }"
             >Контакты
           </router-link>
         </li>
         <li
-          class="text-3xl text-red-700 hover:bg-black/20 cursor-pointer w-full text-center"
+          class="py-4 text-3xl text-red-700 hover:bg-black/20 cursor-pointer w-full text-center"
           @click="() => (status = !status)"
         >
           X
